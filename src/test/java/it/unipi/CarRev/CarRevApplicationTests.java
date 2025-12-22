28a6e16144e5b6a44139a390909b9129a05ef2a6
rvelasco2000 <r.velasco@studenti.unipi.it>
2025-12-22 11:23:43 +0100

    ho

---
+++ src/test/java/it/unipi/CarRev/CarRevApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.CarRev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
