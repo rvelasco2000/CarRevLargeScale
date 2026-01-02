f9989a744d5daca7c82a0622e4f85278ffccda7d
rvelasco2000 <r.velasco@studenti.unipi.it>
2026-01-02 16:51:01 +0100

    commit

--- src/main/java/it/unipi/CarRev/service/Impl/BotDetectionService.java
+++ src/main/java/it/unipi/CarRev/service/Impl/BotDetectionService.java
@@ -9,8 +9,8 @@ import redis.clients.jedis.params.SetParams;
 public class BotDetectionService {
 
     private static final int EXPIRESTATUSKEY=86400; /*the key will expire after a day*/
-    private static final int EXPIRENOFVISITEDPAGES=900; /*change this if not working*/
-    private static final int TRESHOLD=4;
+    private static final int EXPIRENOFVISITEDPAGES=1; /*change this if not working*/
+    private static final int TRESHOLD=100;
     public Boolean checkForBot(String idUser){
         String statusKey="TrafficLog:"+idUser+":status";
         String nOfVisitedPagesKey="TrafficLog:"+idUser+":numberOfVisitedPages";
