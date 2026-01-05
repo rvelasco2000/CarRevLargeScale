998685e91f034acd3d8f7117e54e476812103360
rvelasco2000 <r.velasco@studenti.unipi.it>
2026-01-05 17:52:56 +0100

    starting the 5 most recent car features

--- src/main/java/it/unipi/CarRev/controller/CarController.java
+++ src/main/java/it/unipi/CarRev/controller/CarController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import tools.jackson.core.util.RecyclerPool;
 
 @RestController
 @RequestMapping("/api/cars")
@@ -53,4 +54,7 @@ public class CarController {
         }
         return ResponseEntity.ok(fullCarInfoDTO);
     }
+    /*
+    @GetMapping("/logged/lastFive")
+    public ResponseEntity<>*/
 }
--- src/main/java/it/unipi/CarRev/service/Impl/VisitACarService.java
+++ src/main/java/it/unipi/CarRev/service/Impl/VisitACarService.java
@@ -8,6 +8,9 @@ import it.unipi.CarRev.model.Car;
 import it.unipi.CarRev.utils.CarUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import redis.clients.jedis.Jedis;
 
@@ -26,6 +29,11 @@ public class VisitACarService {
             return null;
         }
         cachedViewsForCarService.updateCachedViewsForCar(id);
+        //if the user is logged i need to save some info about the car into redis
+        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
+        if(auth != null && auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken)){
+            //i will insert here the logic for redis save
+        }
         return CarUtils.mapCarToDto(car);
     }
 }
