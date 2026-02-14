c0547adbe302772021c5b4df9086574a8399d4ef
rvelasco2000 <r.velasco@studenti.unipi.it>
2026-02-14 16:31:53 +0100

    implemented see the reviews of a car

---
+++ src/main/java/it/unipi/CarRev/controller/GeneralController.java
@@ -0,0 +1,38 @@
+package it.unipi.CarRev.controller;
+
+
+import it.unipi.CarRev.dto.UserReviewResponseDTO;
+import it.unipi.CarRev.service.Impl.ShowReviewOfAUserServiceImplementation;
+import it.unipi.CarRev.service.exception.ResourceNotFoundException;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api/general")
+public class GeneralController {
+    private final ShowReviewOfAUserServiceImplementation showReviewOfAUserServiceImplementation;
+    public GeneralController(ShowReviewOfAUserServiceImplementation showReviewOfAUserServiceImplementation){
+        this.showReviewOfAUserServiceImplementation=showReviewOfAUserServiceImplementation;
+    }
+    @GetMapping("/userRev")
+    private ResponseEntity<?> getUserReviews(@NotNull @RequestParam String username,@NotNull @RequestParam Integer numPage){
+        try{
+            Page<UserReviewResponseDTO> userReviewResponseDTOS=showReviewOfAUserServiceImplementation.getAllReviewsFromUsers(username,numPage);
+            return ResponseEntity.ok(userReviewResponseDTOS);
+
+        }
+        catch (ResourceNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+        catch(RuntimeException e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("an error has occurred while fetching the user reviews");
+        }
+    }
+
+
+}
